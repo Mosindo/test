864cb57a626e81ec9b93ef000df451989e358a18
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-02-22 23:39:34 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ScrollView,Image,Pressable,ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.container2}> 
+          <Text style={styles.stylingText}>  
+            working on your app!
+          </Text>
+          <Text style={styles.stylingText}> 
+            working on your app!
+          </Text>
+          <Text style={styles.stylingText}>
+            working on your app
+          </Text>
+          <StatusBar style="auto"/>
+          <Image source={require("./img/konexio-logo_1.png")} 
+	    style={{ width: 900, height: 200 }} />
+          <Image
+        source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
+        style={{ width: 100, height: 100 }} />
+          <Pressable
+            onPress={() => {
+            alert('pressed')
+            }}>
+          </Pressable>
+          <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  container2: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+   
+    // justifyContent: 'center',
+  },
+  stylingText:{
+    marginVertical: 80,
+    fontSize:30,
+    fontWeight:'bold',
+  }
 });
